36fbae3ae932a8190c1b686ca3f5d955aa9af950
ebao <e.nao.0921.nao.e@gmail.com>
2024-05-04 20:59:40 +0900

    Fix all files

--- tnk_quest_front/src/TnkQuestApp.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TnkQuestApp from './TnkQuestApp';
-
-test('renders TnkQuest header', () => {
-  render(<TnkQuestApp />);
-  const linkElement = screen.getByText(/TnkQuest/i);
-  expect(linkElement).toBeInTheDocument();
-});
